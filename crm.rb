55a60d79c884e78a7ea7828a0443bfd93f4892ef
Jon <jonathankrista@gmail.com>
2015-11-09 23:56:13 -0500

    commiting

--- crm.rb
+++ crm.rb
@@ -1,6 +1,23 @@
 
 require 'sinatra'
-require_relative 'contact'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+
+class Contact
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :note, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -8,6 +25,7 @@ get '/' do
 end
 
 get "/contacts" do
+  @contacts = Contact.all
   erb :contacts
 end
 
@@ -19,18 +37,8 @@ get '/contacts/delete' do
 	erb :delete_contact
 end
 
-get '/contacts/delete' do
-	erb :delete_contact
-end
-
-post '/contacts' do
-	puts params
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-  redirect to('/contacts')
-end
-
 get "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
   	erb :show_contact
   else
@@ -39,7 +47,7 @@ get "/contacts/:id" do
 end
 
 get "/contacts/:id/edit" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :edit_contact
   else
@@ -48,26 +56,63 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
-  if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
-
-    redirect to("/contacts")
+  @contact = Contact.get(params[:id].to_i)
+  if @contact then
+    @contact.update(
+      :first_name => params[:first_name],
+      :last_name => params[:last_name],
+      :email => params[:email],
+      :note => params[:note])
+    redirect to("/contacts/#{@contact.id}")
   else
     raise Sinatra::NotFound
   end
 end
 
 delete "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    @contact.remove
+    @contact.destroy
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
   end
 end
+
+
+
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note])
+
+  redirect to('/contacts')
+end
+
+
+
+
+# require 'sinatra'
+#   DataMapper.setup(:default, 'sqlite:crm_development.db')
+
+
+# class Contact
+#   include Datamapper::Resource
+#   # will figure this out to mean 'contacts' table i
+#   perperty :id, Serial
+#   property :first_name, String
+#   property :last_name, String
+#   perperty :email, String
+#   peoperty :note, Text
+# end
+
+# DataMapper.auto.method!
+
+# gets '/contacts ' do
+#   @contacts = contact.all
+
+
+
 
